983e59e816c6045644c5985aff829fe0118f711c
SebastianHafner <hafnersailing@gmail.com>
2023-01-27 16:34:42 +0100

    architecture without consistency loss

--- train_supervised_multimodal_dualtask.py
+++ train_supervised_multimodal_dualtask.py
@@ -6,7 +6,6 @@ import torch
 from torch import optim
 from torch.utils import data as torch_data
 
-from tabulate import tabulate
 import wandb
 import numpy as np
 
@@ -14,18 +13,6 @@ from utils import networks, datasets, loss_functions, evaluation, experiment_man
 
 
 def run_training(cfg):
-    run_config = {
-        'CONFIG_NAME': cfg.NAME,
-        'device': device,
-        'epochs': cfg.TRAINER.EPOCHS,
-        'learning rate': cfg.TRAINER.LR,
-        'batch size': cfg.TRAINER.BATCH_SIZE,
-    }
-    table = {'run config name': run_config.keys(),
-             ' ': run_config.values(),
-             }
-    print(tabulate(table, headers='keys', tablefmt="fancy_grid", ))
-
     net = networks.create_network(cfg)
     net.to(device)
     optimizer = optim.AdamW(net.parameters(), lr=cfg.TRAINER.LR, weight_decay=0.01)
@@ -33,7 +20,7 @@ def run_training(cfg):
     criterion = loss_functions.get_criterion(cfg.MODEL.LOSS_TYPE)
 
     # reset the generators
-    dataset = datasets.MultimodalCDDataset(cfg=cfg, run_type='training')
+    dataset = datasets.MultimodalCDDataset(cfg=cfg, run_type='train')
     print(dataset)
 
     dataloader_kwargs = {
@@ -47,11 +34,14 @@ def run_training(cfg):
 
     # unpacking cfg
     epochs = cfg.TRAINER.EPOCHS
-    save_checkpoints = cfg.SAVE_CHECKPOINTS
     steps_per_epoch = len(dataloader)
 
     # tracking variables
     global_step = epoch_float = 0
+
+    # early stopping
+    best_f1_val, trigger_times = 0, 0
+    stop_training = False
 
     for epoch in range(1, epochs + 1):
         print(f'Starting epoch {epoch}/{epochs}.')
@@ -74,24 +64,24 @@ def run_training(cfg):
             logits_fusion_sem_t1, logits_fusion_sem_t2 = logits[5:]
 
             # change detection
-            gt_change = batch['y_change'].to(device)
-            change_loss = criterion(logits_change, gt_change)
+            y_change = batch['y_change'].to(device)
+            change_loss = criterion(logits_change, y_change)
 
             # semantics
-            gt_sem_t1 = batch['y_sem_t1'].to(device)
-            sem_stream1_t1_loss = criterion(logits_stream1_sem_t1, gt_sem_t1)
-            sem_stream2_t1_loss = criterion(logits_stream2_sem_t1, gt_sem_t1)
-            sem_fusion_t1_loss = criterion(logits_fusion_sem_t1, gt_sem_t1)
+            y_sem_t1 = batch['y_sem_t1'].to(device)
+            sem_stream1_t1_loss = criterion(logits_stream1_sem_t1, y_sem_t1)
+            sem_stream2_t1_loss = criterion(logits_stream2_sem_t1, y_sem_t1)
+            sem_fusion_t1_loss = criterion(logits_fusion_sem_t1, y_sem_t1)
 
-            gt_sem_t2 = batch['y_sem_t2'].to(device)
-            sem_stream1_t2_loss = criterion(logits_stream1_sem_t2, gt_sem_t2)
-            sem_stream2_t2_loss = criterion(logits_stream2_sem_t2, gt_sem_t2)
-            sem_fusion_t2_loss = criterion(logits_fusion_sem_t2, gt_sem_t2)
+            y_sem_t2 = batch['y_sem_t2'].to(device)
+            sem_stream1_t2_loss = criterion(logits_stream1_sem_t2, y_sem_t2)
+            sem_stream2_t2_loss = criterion(logits_stream2_sem_t2, y_sem_t2)
+            sem_fusion_t2_loss = criterion(logits_fusion_sem_t2, y_sem_t2)
 
             sem_loss = (sem_stream1_t1_loss + sem_stream1_t2_loss + sem_stream2_t1_loss + sem_stream2_t2_loss +
-                        sem_fusion_t1_loss + sem_fusion_t2_loss)
+                        sem_fusion_t1_loss + sem_fusion_t2_loss) / 6
 
-            loss = change_loss + sem_loss
+            loss = change_loss + sem_loss / 2
 
             change_loss_set.append(change_loss.item())
             sem_loss_set.append(sem_loss.item())
@@ -103,17 +93,8 @@ def run_training(cfg):
             global_step += 1
             epoch_float = global_step / steps_per_epoch
 
-            if cfg.DEBUG:
-                break
-
-            if global_step % cfg.LOG_FREQ == 0:
+            if global_step % cfg.LOGGING.FREQUENCY == 0:
                 print(f'Logging step {global_step} (epoch {epoch_float:.2f}).')
-
-                # evaluation on sample of training and validation set
-                evaluation.model_evaluation_mm_dt(net, cfg, device, 'training', epoch_float, global_step)
-                evaluation.model_evaluation_mm_dt(net, cfg, device, 'validation', epoch_float, global_step)
-
-                # logging
                 time = timeit.default_timer() - start
                 wandb.log({
                     'change_loss': np.mean(change_loss_set) if len(change_loss_set) > 0 else 0,
@@ -125,21 +106,30 @@ def run_training(cfg):
                     'epoch': epoch_float,
                 })
                 start = timeit.default_timer()
-                n_labeled, n_notlabeled = 0, 0
-                change_loss_set, sem_loss_set, sup_loss_set, cons_loss_set, loss_set = [], [], [], [], []
+                change_loss_set, sem_loss_set, loss_set = [], [], []
             # end of batch
 
-        if not cfg.DEBUG:
-            assert (epoch == epoch_float)
+        assert (epoch == epoch_float)
         print(f'epoch float {epoch_float} (step {global_step}) - epoch {epoch}')
         # evaluation at the end of an epoch
-        evaluation.model_evaluation_mm_dt(net, cfg, device, 'training', epoch_float, global_step)
-        evaluation.model_evaluation_mm_dt(net, cfg, device, 'validation', epoch_float, global_step)
-        evaluation.model_evaluation_mm_dt(net, cfg, device, 'test', epoch_float, global_step)
+        _ = evaluation.model_evaluation_mm_dt(net, cfg, 'train', epoch_float, global_step)
+        f1_val = evaluation.model_evaluation_mm_dt(net, cfg, 'val', epoch_float, global_step)
 
-        if epoch in save_checkpoints:
-            print(f'saving network', flush=True)
-            networks.save_checkpoint(net, optimizer, epoch, global_step, cfg)
+        if f1_val <= best_f1_val:
+            trigger_times += 1
+            if trigger_times > cfg.TRAINER.PATIENCE:
+                stop_training = True
+        else:
+            best_f1_val = f1_val
+            print(f'saving network (F1 {f1_val:.3f})', flush=True)
+            networks.save_checkpoint(net, optimizer, epoch, cfg)
+            trigger_times = 0
+
+        if stop_training:
+            break  # end of training by early stopping
+
+    net, *_ = networks.load_checkpoint(cfg, device)
+    _ = evaluation.model_evaluation_mm_dt(net, cfg, 'test', epoch_float, global_step)
 
 
 if __name__ == '__main__':
