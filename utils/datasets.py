72f2f3361a1b4ea33ce821390ee280d3b5732812
SebastianHafner <hafnersailing@gmail.com>
2023-05-01 01:37:43 +0200

    yay

--- utils/datasets.py
+++ utils/datasets.py
@@ -214,3 +214,160 @@ class MultimodalCDDataset(AbstractMultimodalCDDataset):
 
     def __str__(self):
         return f'Dataset with {self.length} samples.'
+
+
+class StockholmCDDataset(AbstractMultimodalCDDataset):
+
+    def __init__(self, cfg: experiment_manager.CfgNode, run_type: str, sthlm_data_path: str,
+                 no_augmentations: bool = False, dataset_mode: str = None, disable_multiplier: bool = False,
+                 disable_unlabeled: bool = False):
+        super().__init__(cfg, run_type)
+
+        self.sthlm_data_path = Path(sthlm_data_path)
+        self.sthlm_dates = list(cfg.DATALOADER.STOCKHOLM_DATES)
+        self.dataset_mode = cfg.DATALOADER.DATASET_MODE if dataset_mode is None else dataset_mode
+        self.include_building_labels = cfg.DATALOADER.INCLUDE_BUILDING_LABELS
+
+        # handling transformations of data
+        self.no_augmentations = no_augmentations
+        self.transform = augmentations.compose_transformations(cfg, no_augmentations)
+
+        # loading labeled samples (sn7 train set) and subset to run type aoi ids
+        if run_type == 'train':
+            self.aoi_ids = list(cfg.DATASET.TRAIN_IDS)
+        elif run_type == 'val':
+            self.aoi_ids = list(cfg.DATASET.VALIDATION_IDS)
+        elif run_type == 'test':
+            self.aoi_ids = list(cfg.DATASET.TEST_IDS)
+        else:
+            raise Exception('Unknown run type!')
+
+        self.labeled = [True] * len(self.aoi_ids)
+
+        if not disable_multiplier:
+            self.aoi_ids = self.aoi_ids * cfg.DATALOADER.TRAINING_MULTIPLIER
+            self.labeled = self.labeled * cfg.DATALOADER.TRAINING_MULTIPLIER
+
+        # include unlabeled data from stockholm
+        if cfg.DATALOADER.INCLUDE_UNLABELED and not disable_unlabeled:
+            assert (run_type == 'train')
+            assert(cfg.AUGMENTATION.CROP_SIZE <= 256)
+            self.tiles_stockholm = geofiles.load_json(self.sthlm_data_path / f'tiles_stockholm.json')
+            self.aoi_ids.extend(self.tiles_stockholm)
+            self.labeled.extend([False] * len(self.tiles_stockholm))
+
+        manager = multiprocessing.Manager()
+        self.unlabeled_ids = manager.list(list(self.cfg.DATASET.UNLABELED_IDS))
+        self.aoi_ids = manager.list(self.aoi_ids)
+        self.labeled = manager.list(self.labeled)
+        self.metadata = manager.dict(self.metadata)
+
+        self.length = len(self.aoi_ids)
+
+    def __getitem__(self, index):
+
+        aoi_id = self.aoi_ids[index]
+        labeled = self.labeled[index]
+
+        if 'stockholm' in aoi_id:
+
+            patch_id = aoi_id.split('_')[-1]
+            indices = sorted(np.random.randint(0, len(self.sthlm_dates), size=2))
+            # t1
+            date_t1 = self.sthlm_dates[indices[0]]
+            img_s1_t1 = self._load_s1_img_sthlm(date_t1, patch_id)
+            img_s2_t1 = self._load_s2_img_sthlm(date_t1, patch_id)
+
+            # t2
+            date_t2 = self.sthlm_dates[indices[-1]]
+            img_s1_t2 = self._load_s1_img_sthlm(date_t2, patch_id)
+            img_s2_t2 = self._load_s2_img_sthlm(date_t2, patch_id)
+
+            change = np.zeros((img_s1_t1.shape[0], img_s1_t1.shape[1], 1), dtype=np.float32)
+            buildings = np.zeros((change.shape[0], change.shape[1], 2), dtype=np.float32)
+
+        else:
+            timestamps = self.metadata[aoi_id]
+            if labeled:
+                timestamps = [(ts['year'], ts['month']) for ts in timestamps if
+                              ts['s1'] and ts['s2'] and ts['buildings'] and not ts['masked']]
+            else:
+                timestamps = [(ts['year'], ts['month']) for ts in timestamps if ts['s1'] and ts['s2']]
+
+            if self.dataset_mode == 'first_last':
+                indices = [0, -1]
+            else:
+                indices = sorted(np.random.randint(0, len(timestamps), size=2))
+
+            # t1
+            year_t1, month_t1 = timestamps[indices[0]]
+            img_s1_t1 = self._load_s1_img(aoi_id, year_t1, month_t1)
+            img_s2_t1 = self._load_s2_img(aoi_id, year_t1, month_t1)
+
+            # t2
+            year_t2, month_t2 = timestamps[indices[1]]
+            img_s1_t2 = self._load_s1_img(aoi_id, year_t2, month_t2)
+            img_s2_t2 = self._load_s2_img(aoi_id, year_t2, month_t2)
+
+            change = self._load_change_label(aoi_id, year_t1, month_t1, year_t2, month_t2)
+            if self.include_building_labels:
+                buildings_t1 = self._load_building_label(aoi_id, year_t1, month_t1)
+                buildings_t2 = self._load_building_label(aoi_id, year_t2, month_t2)
+                buildings = np.concatenate((buildings_t1, buildings_t2), axis=-1).astype(np.float32)
+            else:
+                buildings = np.zeros((change.shape[0], change.shape[1], 2), dtype=np.float32)
+
+        # transformation, but this ain't pretty
+        imgs = np.concatenate((img_s1_t1, img_s1_t2, img_s2_t1, img_s2_t2), axis=-1)
+        imgs, buildings, change = self.transform((imgs, buildings, change))
+        imgs_s1 = imgs[:2 * len(self.s1_band_indices), ]
+        img_s1_t1, img_s1_t2 = imgs_s1[:len(self.s1_band_indices), ], imgs_s1[len(self.s1_band_indices):, ]
+        imgs_s2 = imgs[2 * len(self.s1_band_indices):, ]
+        img_s2_t1, img_s2_t2 = imgs_s2[:len(self.s2_band_indices), ], imgs_s2[len(self.s2_band_indices):, ]
+
+        if self.cfg.DATALOADER.INPUT_MODE == 's1':
+            x_t1, x_t2 = img_s1_t1, img_s1_t2
+        elif self.cfg.DATALOADER.INPUT_MODE == 's2':
+            x_t1, x_t2 = img_s2_t1, img_s2_t2
+        else:
+            x_t1 = torch.concat((img_s1_t1, img_s2_t1), dim=0)
+            x_t2 = torch.concat((img_s1_t2, img_s2_t2), dim=0)
+
+        item = {
+            'x_t1': x_t1,
+            'x_t2': x_t2,
+            'y_change': change,
+            'aoi_id': aoi_id,
+            'is_labeled': labeled,
+        }
+
+        if self.include_building_labels:
+            buildings_t1, buildings_t2 = buildings[0,], buildings[1,]
+            item['y_sem_t1'] = buildings_t1.unsqueeze(0)
+            item['y_sem_t2'] = buildings_t2.unsqueeze(0)
+
+        return item
+
+    def get_index(self, aoi_id: str) -> int:
+        for index, candidate_aoi_id in enumerate(self.aoi_ids):
+            if aoi_id == candidate_aoi_id:
+                return index
+        return None
+
+    def __len__(self):
+        return self.length
+
+    def __str__(self):
+        return f'Dataset with {self.length} samples.'
+
+    def _load_s1_img_sthlm(self, date: str, patch_id: str) -> np.ndarray:
+        file = self.sthlm_data_path / f's1_stockholm_{date}' / f's1_stockholm_{date}-{patch_id}.tif'
+        img, _, _ = geofiles.read_tif(file)
+        img = np.clip(img[:, :, self.s1_band_indices], 0, 1)
+        return np.nan_to_num(img).astype(np.float32)
+
+    def _load_s2_img_sthlm(self, date: str, patch_id: str) -> np.ndarray:
+        file = self.sthlm_data_path / f's2_stockholm_{date}' / f's2_stockholm_{date}-{patch_id}.tif'
+        img, _, _ = geofiles.read_tif(file)
+        img = np.clip(img[:, :, self.s2_band_indices], 0, 1)
+        return np.nan_to_num(img).astype(np.float32)
